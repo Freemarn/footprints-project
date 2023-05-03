a78eb303d8f7b93461f34f65fb96debdc3f6f7cb
stickmandev <stickmandeveloper@gmail.com>
2023-05-03 22:33:29 +0100

    temporal romove of jazmin

--- footprints/settings.py
+++ footprints/settings.py
@@ -174,41 +174,41 @@ SIMPLE_JWT = {
 
 AUTH_USER_MODEL = 'footUsers.FootUser'
 
-JAZZMIN_UI_TWEAKS = {
-    # "theme": "darkly",
-    # "theme": "simplex",   
-    # "theme": "sketchy",
-    # "theme": "slate",
-}
+# JAZZMIN_UI_TWEAKS = {
+#     # "theme": "darkly",
+#     # "theme": "simplex",   
+#     # "theme": "sketchy",
+#     # "theme": "slate",
+# }
 
-JAZZMIN_SETTINGS = {
-    # title of the window (Will default to current_admin_site.site_title if absent or None)
-    "site_title": "Foot Prints",
+# JAZZMIN_SETTINGS = {
+#     # title of the window (Will default to current_admin_site.site_title if absent or None)
+#     "site_title": "Foot Prints",
 
-    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_header": "Foot Prints",
+#     # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+#     "site_header": "Foot Prints",
 
-    # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    "site_brand": "Foot Prints",
+#     # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+#     "site_brand": "Foot Prints",
 
-    # Logo to use for your site, must be present in static files, used for brand on top left
-    # "site_logo": "books/img/website_photography-1_1_Wnw2vGM.jpg",
+#     # Logo to use for your site, must be present in static files, used for brand on top left
+#     # "site_logo": "books/img/website_photography-1_1_Wnw2vGM.jpg",
 
-    # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
-    "login_logo": None,
+#     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
+#     "login_logo": None,
 
-    # Logo to use for login form in dark themes (defaults to login_logo)
-    "login_logo_dark": None,
+#     # Logo to use for login form in dark themes (defaults to login_logo)
+#     "login_logo_dark": None,
 
-    # CSS classes that are applied to the logo above
-    "site_logo_classes": "img-circle",
+#     # CSS classes that are applied to the logo above
+#     "site_logo_classes": "img-circle",
 
-    # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
-    "site_icon": None,
+#     # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
+#     "site_icon": None,
 
-    # Welcome text on the login screen
-    "welcome_sign": "Welcome to Foot Prints",
+#     # Welcome text on the login screen
+#     "welcome_sign": "Welcome to Foot Prints",
 
-    # Copyright on the footer
-    "copyright": "Foot Prints",
-}
+#     # Copyright on the footer
+#     "copyright": "Foot Prints",
+# }
